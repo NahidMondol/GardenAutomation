88347dd868c21e6d1a6cc34aca97f81337188d90
Nahid Mondol <nahidmondol97@gmail.com>
2021-07-29 03:41:28 +0100

    Modify the way a connection is made to the db

--- database.py
+++ database.py
@@ -1,18 +1,10 @@
 import psycopg2
+from config import config
 
 class GardenDatabase:
-    DB_HOST = "192.168.0.41"
-    DB_NAME = "gardendb"
-    DB_USER = "pi"
-    DB_PASSWORD = "6201"
-    conn = None
 
     def __init__(self):
-        self.DB_HOST = "192.168.0.41"
-        self.DB_NAME = "gardendb"
-        self.DB_USER = "pi"
-        self.DB_PASSWORD = "6201"
-        #self.conn = psycopg2.connect(dbname = self.DB_NAME, user = self.DB_USER, password = self.DB_PASSWORD, host = self.DB_HOST)
+        pass
 
 
     def convertCelsiusToFahrenheit(tempInCelsius):
@@ -20,12 +12,14 @@ class GardenDatabase:
 
 
     def getAll(self):
+        conn = None
         try:
-            conn = psycopg2.connect(dbname = self.DB_NAME, user = self.DB_USER, password = self.DB_PASSWORD, host = self.DB_HOST)
+            params = config()
+            conn = psycopg2.connect(**params)
             cur = conn.cursor()
             cur.execute("SELECT * FROM general;")
             list = cur.fetchall()
-            conn.close()
+            cur.close()
             return list
         finally:
             if conn is not None:
@@ -33,12 +27,14 @@ class GardenDatabase:
 
 
     def insertData(self, temperature, humidity):
+        conn = None
         try:
-            conn =  psycopg2.connect(dbname = self.DB_NAME, user = self.DB_USER, password = self.DB_PASSWORD, host = self.DB_HOST)
+            params = config()
+            conn =  psycopg2.connect(**params)
             cur = conn.cursor()
-            cur.execute("INSERT INTO general (recorded_timestamp, temperature, soil_humidity) VALUES (current_timestamp, %s, %s);", (GardenDatabse.convertCelsiusToFahrenheit(temperature), humidity))
+            cur.execute("INSERT INTO general (recorded_timestamp, temperature, soil_humidity) VALUES (current_timestamp, %s, %s);", (GardenDatabase.convertCelsiusToFahrenheit(temperature), humidity))
             conn.commit()
-            conn.close()
+            cur.close()
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
         finally:
@@ -47,13 +43,15 @@ class GardenDatabase:
 
 
     def getTimeTempAndHumidity(self):
+        conn = None
         try:
-            conn =  psycopg2.connect(dbname = self.DB_NAME, user = self.DB_USER, password = self.DB_PASSWORD, host = self.DB_HOST)
+            params = config()
+            conn = psycopg2.connect(**params)
             cur = conn.cursor()
             cur.execute("SELECT general_id, recorded_timestamp, temperature, soil_humidity FROM general;")
             list = cur.fetchall()
             for item in list:
-                print("ID {}\nTime {}\nTemperature {}\nHumidity {}".format(item[0], item[1], GardenDatabase.convertCelsiusToFahrenheit(item[2]), item[3]))
+                print("ID {}\nTime {}\nTemperature {}\nHumidity {}".format(item[0], item[1], item[2], item[3]))
             conn.close()
             return list
         except (Exception, psycopg2.DatabaseError) as error:
